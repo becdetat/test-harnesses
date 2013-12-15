5225a6c404fcf5bae51fc735d5bc9492b018262d
Ben Scott <ben.scott@readify.net>
2013-12-15 11:04:54 +1000

    Add concernfor class to nunit project

---
+++ nunit/nunit-test-harness/Infrastructure/ConcernFor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nunit_test_harness.Infrastructure
+{
+    public abstract class ConcernFor<T> : IDisposable
+    {
+        protected T Subject;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            Context();
+            Subject = Given();
+            When();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            Dispose();
+        }
+
+        protected virtual void Context()
+        {
+        }
+
+        protected abstract T Given();
+
+        protected virtual void When()
+        {
+        }
+
+        public virtual void Dispose()
+        {
+        }
+    }
+}
---
+++ nunit/nunit-test-harness/Infrastructure/ConcernForTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nunit_test_harness.Infrastructure
+{
+    public class Foo
+    {
+    }
+
+    [TestFixture]
+    public class FooTests : ConcernFor<Foo>
+    {
+        private bool _contextHasBeenExecuted;
+        private bool _givenHasBeenExecuted;
+        private bool _whenHasBeenExecuted;
+        private bool _disposeHasBeenExecuted;
+
+        protected override void Context()
+        {
+            _contextHasBeenExecuted = true;
+        }
+
+        protected override Foo Given()
+        {
+            _givenHasBeenExecuted = true;
+
+            return new Foo();
+        }
+
+        protected override void When()
+        {
+            _whenHasBeenExecuted = true;
+        }
+
+        public override void Dispose()
+        {
+            _disposeHasBeenExecuted = true;
+        }
+
+        [Test]
+        public void ContextShouldBeExecuted()
+        {
+            Assert.IsTrue(_contextHasBeenExecuted);
+        }
+
+        [Test]
+        public void GivenShouldBeExecuted()
+        {
+            Assert.IsTrue(_givenHasBeenExecuted);
+        }
+
+        [Test]
+        public void SubjectShouldBeAssigned()
+        {
+            Assert.IsNotNull(Subject);
+        }
+
+        [Test]
+        public void WhenShouldBeExecuted()
+        {
+            Assert.IsTrue(_whenHasBeenExecuted);
+        }
+
+        [Test]
+        public void DisposeShouldBeExecuted()
+        {
+            Assert.IsTrue(_disposeHasBeenExecuted);
+        }
+    }
+}
